f5fe3aaf5b23b673767e8fceab56b45d5d972540
Chirag <140259132+chiraglaheru@users.noreply.github.com>
2025-10-14 00:25:22 +0530

    Update ceaser.py

--- ceaser.py
+++ ceaser.py
@@ -1,5 +1,7 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
+
 def encrypt(original_text, shift_amount):
     cipher_text = ""
     for letter in original_text:
@@ -22,10 +24,19 @@ def decrypt(original_text, shift_amount):
             output_text += letter
     print(f"Here is the decoded result: {output_text}")
 
+should_continue = True
+
+while should_continue:
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type your shift number:\n"))
+
+restart = input("type 'yes if you want to go again or type 'no'\n").lower()
+if restart ==  "no":
+    should_continue = False
+    print("Get out!!! :)")
+    
 
 if direction == "encode":
     encrypt(original_text=text, shift_amount=shift)
@@ -37,7 +48,9 @@ else:
 def ceaser(original_text, shift_amount, encode_or_decode):
     output_text = ""
     
-
+    if letter in original_text:
+        output_text += letter
+    else:
     if encode_or_decode == "decode":
         shift_amount *= -1
     
